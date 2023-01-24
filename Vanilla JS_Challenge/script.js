049a68f24fa8c8428f2a2795612a8d9c9ebd9722
ssum21 <ssumuss@khu.ac.kr>
2023-01-24 10:18:03 +0900

    230124 My first JS Challenge

--- Vanilla JS_Challenge/script.js
+++ Vanilla JS_Challenge/script.js
@@ -1,1 +1,26 @@
-alert("Hi");
+let a = 5; //After creation, we can change the value
+const b = 2; // After creation, we can't change the value
+const myName = "nico"; //JS에서는 띄어쓰기 이후의 글자는 대문자로 쓰자.
+//기본적으로는 const를 사용하고, 변경사항이 자주있는 일부 변수에 대해서만 let을 사용하는 것이 관용.
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello"+myName);
+
+const amIFat = true;
+console.log(amIFat);
+
+const amINull = null; //null은 자연스럽게 발생할 수 X
+console.log = amINull;
+
+let something;
+console.log = something; //undefined 상태, 메모리는 할당되어 있음.
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+console.log(daysOfWeek[5]);
+
+// Add one more day to the array
+daysOfWeek.push("sum");
+console.log(daysOfWeek);
---
+++ Vanilla JS_Challenge/scriptObject.js
@@ -0,0 +1,19 @@
+const playerName = "ssum";
+const playerPoints = 121212;
+const playerHandsome = true;
+const playerFat = "litter bit";
+
+const player1 = ["nico", 1212, false, "little bit"];
+
+const player2 = {
+    name: "nico",
+    points : 10,
+    fat: true,
+};
+
+console.log(player2);
+console.log(player2.name);
+
+player2.lastName = "potato";
+player2.points = 15;
+console.log(player2);
