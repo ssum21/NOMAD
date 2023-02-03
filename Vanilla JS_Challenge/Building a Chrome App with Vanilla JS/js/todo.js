c41390b4adf1162ac05e260ffdb20c3365e1e8fe
ssum21 <ssumuss@khu.ac.kr>
2023-02-03 10:38:10 +0900

    230203 Adding ToDos

--- Vanilla JS_Challenge/Building a Chrome App with Vanilla JS/js/todo.js
+++ Vanilla JS_Challenge/Building a Chrome App with Vanilla JS/js/todo.js
@@ -1,9 +1,19 @@
 const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-function handleToDoSubmit(event){
-    event.preventdefault
+function paintToDo(newTodo){
+    const li = document.createElement("li")
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
 
+function handleToDoSubmit(event){
+    event.preventdefault();
+    const newTodo = toDoInput.ariaValueMax;
+    toDoInput.value = "";
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
