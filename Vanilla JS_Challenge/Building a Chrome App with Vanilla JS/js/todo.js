53f9622bd3d9ff94ee98d435cc63640f1d982877
ssum21 <ssumuss@khu.ac.kr>
2023-02-03 10:28:17 +0900

    230203 Building a Chrome App with Vanilla JS

--- Vanilla JS_Challenge/Building a Chrome App with Vanilla JS/js/todo.js
+++ Vanilla JS_Challenge/Building a Chrome App with Vanilla JS/js/todo.js
@@ -2,7 +2,7 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 
 function handleToDoSubmit(event){
-    
+    event.preventdefault
 
 }
 
