caf27b5a174ace1e49dbd3f15344c496c7e9df53
ssum21 <ssumuss@khu.ac.kr>
2023-02-04 20:57:57 +0900

    Project Making Chrome App, completed modifying

--- Vanilla JS_Challenge/Building a Chrome App with Vanilla JS/js/background.js
+++ Vanilla JS_Challenge/Building a Chrome App with Vanilla JS/js/background.js
@@ -1,9 +1,9 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
+const bgImages = ['1.jpg', '2.jpg', '3.jpg']
+const selectedImg = bgImages[Math.floor(Math.random()*bgImages.length)]
 const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
+const bgImageUrl = `img/${selectedImg}`
+bgImage.classList.add('hidden')
+bgImage.src = bgImageUrl
+document.body.style.backgroundImage = `url(${bgImageUrl})`
+console.log(`url(${bgImageUrl})`)
 document.body.appendChild(bgImage);
